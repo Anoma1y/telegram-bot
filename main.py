51eb9853e9004224b54a266839fd28e0d33758dc
German <ueuknjg@gmail.com>
2018-08-06 18:30:12 +0300

    add tags

--- main.py
+++ main.py
@@ -74,25 +74,26 @@ def invalid_cmd(chat_id, cmd):
 
 # get command list
 def get_help_command_list(chat_id):
-    file = open("telegram_help.txt", 'r', encoding='utf-8')
-    help_doc = ''
-    if file.mode == 'r':
-        help_doc = file.read().encode('UTF-8')
+    help_doc = 'Список доступных команд:\n'
+    help_doc += '\t\t/image - Получить картинку\n'
+    help_doc += '\t\t/help - Список команд\n'
     bot.sendMessage(chat_id=chat_id, text=help_doc)
 
 
 def handle_cmd(command, chat_id):
 
     if command == '/image':
-        send_album(chat_id)
+        send_album(chat_id, command)
     elif command == '/help':
         get_help_command_list(chat_id)
     else:
         invalid_cmd(chat_id, command)
 
 
-def send_album(chat_id):
-    json_data = yandere.get_images(page_limit=3, tags='loli', period_time=unix_time['day'], limit=5)
+def send_album(chat_id, command):
+    command = command.split(' ', maxsplit=1)
+
+    json_data = yandere.get_images(page_limit=3, tags=command[1], period_time=unix_time['day'], limit=5)
 
     for data in json_data:
         bot.sendPhoto(chat_id=chat_id, photo=data['file_url'])
