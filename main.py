4f314146e8e2391a43edc7da4677e53c9e1a6acb
German Voytekhovich <ueuknjg@gmail.com>
2018-08-07 11:08:04 +0300

    remove flask

--- main.py
+++ main.py
@@ -1,56 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from flask import Flask, request
 import telegram
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, run_async
 import methods
 import yandere
-import asyncio
-from time import sleep
 import config as CONFIG
-
-
-app = Flask(__name__)
-app.debug = True
-
-
-global bot
-bot = telegram.Bot(token=CONFIG.TOKEN)
-
-URL = '207.154.250.14'
+from time import sleep
 
 
 
-@app.route('/pudge', methods=['POST', 'GET'])
-def webhook_handler():
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        try:
-            chat_id = update.message.chat.id
-            command = update.message.text
-
-            if command:
-                handle_cmd(command, chat_id)
-            else:
-                invalid_cmd(chat_id=chat_id, cmd=command)
-        except Exception as e:
-            print("type error: " + str(e))
-    return 'ok'
-
-
-@app.route('/set_webhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = bot.setWebhook('https://%s:443/pudge' % URL, certificate=open('/etc/ssl/server.crt', 'rb'))
-    if s:
-        print(s)
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
-
-
-@app.route("/")
-def hello_world():
-    return "Hello World"
+# app = Flask(__name__)
+# app.debug = True
+#
+#
+# global bot
+# bot = telegram.Bot(token=CONFIG.TOKEN)
+#
+# URL = '207.154.250.14'
+#
+#
+# @app.route('/pudge', methods=['POST', 'GET'])
+# def webhook_handler():
+#     if request.method == "POST":
+#         update = telegram.Update.de_json(request.get_json(force=True), bot)
+#         try:
+#             chat_id = update.message.chat.id
+#             command = update.message.text
+#
+#             if command:
+#                 handle_cmd(command, chat_id)
+#             else:
+#                 invalid_cmd(chat_id=chat_id, cmd=command)
+#         except Exception as e:
+#             print("type error: " + str(e))
+#     return 'ok'
 
 
 unix_time = {
@@ -71,7 +54,7 @@ def get_image():
 def invalid_cmd(chat_id, cmd):
     msg = 'Команда ' + cmd + ' не найдена\n'
     msg += 'Для получения списка доступных команд, напишите /help'
-    bot.sendMessage(chat_id=chat_id, text=msg)
+    # bot.sendMessage(chat_id=chat_id, text=msg)
 
 
 # get command list
@@ -80,7 +63,7 @@ def get_help_command_list(chat_id):
     help_doc += '\t\t/image {{tag}} - Получить последние картинки за 1 неделю\n'
     help_doc += '\t\t/tags - Получить все популярные теги\n'
     help_doc += '\t\t/help - Список команд\n'
-    bot.sendMessage(chat_id=chat_id, text=help_doc)
+    # bot.sendMessage(chat_id=chat_id, text=help_doc)
 
 
 def handle_cmd(command, chat_id):
@@ -96,8 +79,8 @@ def handle_cmd(command, chat_id):
         invalid_cmd(chat_id, main_command)
 
 
-def send_album(chat_id, command):
-    tags = command.split(' ', maxsplit=1)
+def send_album(bot, update):
+    tags = update.message.text.split(' ', maxsplit=1)
 
     if len(tags) <= 1:
         tags = None
@@ -105,15 +88,67 @@ def send_album(chat_id, command):
     json_data = yandere.get_images(page_limit=3, tags=tags[1], period_time=unix_time['week'], limit=5)
 
     for data in json_data:
-        bot.sendPhoto(chat_id=chat_id, photo=data['file_url'])
+        pass
+        bot.sendPhoto(chat_id=update.message.chat_id, photo=data['file_url'])
 
 
-def send_tags(chat_id):
+def send_tags(bot, update):
     tags_list = yandere.get_available_tags()
 
     if len(tags_list) > 0:
-        bot.sendMessage(chat_id=chat_id, text=tags_list)
+        pass
+        bot.sendMessage(chat_id=update.message.chat_id, text=tags_list)
 
 
 def error():
     pass
+
+
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
+def textMessage(bot, update):
+    response = 'Получил Ваше сообщение: ' + update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+
+
+updater = Updater(token=CONFIG.TOKEN)
+dispatcher = updater.dispatcher
+
+
+def gg_lol(bot, update):
+    while True:
+        print('Hui')
+        sleep(2)
+
+
+@run_async
+def starter(bot, update):
+    gg_lol(bot, update)
+
+
+def main():
+    try:
+        text_message_handler = MessageHandler(Filters.text, textMessage)
+        start_command_handler = CommandHandler('start', starter)
+        tags_command_handler = CommandHandler('tags', send_tags)
+        image_command_handler = CommandHandler('image', send_album)
+
+        dispatcher.add_handler(start_command_handler)
+        dispatcher.add_handler(tags_command_handler)
+        dispatcher.add_handler(image_command_handler)
+        dispatcher.add_handler(text_message_handler)
+
+        updater.start_polling(clean=True)
+
+        updater.idle()
+    except Exception as e:
+        print("type error: " + str(e))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
